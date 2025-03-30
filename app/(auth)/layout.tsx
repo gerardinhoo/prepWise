4a52c55d8cde11a7fea438bc5a3fcf23140f889a
Gerard Eklu <gerard.eklu@yahoo.fr>
2025-03-30 00:13:09 -0400

    Implemented the auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ReactNode } from 'react';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className='auth-layout'>{children}</div>;
 };
 
 export default AuthLayout;
